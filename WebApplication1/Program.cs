e4d6306de643cdca58fc598d37a2d4f80b29de23
Dasha Karpach <karpach.dasha.pl@gmail.com>
2023-04-11 12:35:10 +0200

    Add Services

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,5 +1,10 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
 
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
