5fa40bd769129afcf6ad13b0c08a839c5e8faf44
Dasha Karpach <karpach.dasha.pl@gmail.com>
2023-04-17 11:17:02 +0200

    Add DB Migrations

---
+++ ZetaTrading/Migrations/20230417091429_CreateDB.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ZetaTrading.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "JournalRecords",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RequestId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    StackTrace = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BodyParameters = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    QueryParameters = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Path = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JournalRecords", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TreeNodes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TreeName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ParentNodeId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TreeNodes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TreeNodes_TreeNodes_ParentNodeId",
+                        column: x => x.ParentNodeId,
+                        principalTable: "TreeNodes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TreeNodes_ParentNodeId",
+                table: "TreeNodes",
+                column: "ParentNodeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "JournalRecords");
+
+            migrationBuilder.DropTable(
+                name: "TreeNodes");
+        }
+    }
+}
